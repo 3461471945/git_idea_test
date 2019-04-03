d06de5940ae3fbf919e0c11c491ea48e8b9bbbec
lihaodong <1071888310@qq.com>
2019-04-03 19:00:48 +0800

    提交

---
+++ gittest/src/com/itheima/GitTest.java
@@ -0,0 +1,23 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+
+
+    public void text(){
+         System.out.println("asd ");
+    }
+}
